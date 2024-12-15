cbfbe117e5fd437d0ce3ccffbe4f855c8dba2397
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-12-15 23:19:35 +0700

    Init data and BSS

--- src/crt0.c
+++ src/crt0.c
@@ -1,3 +1,26 @@
+extern char _data_start__[];
+extern char _data_end__[];
+extern char _data_load__[];
+
+extern char _bss_start__[];
+extern char _bss_end__[];
+
+void init_data() {
+  char *src = _data_load__;
+  char *dst = _data_start__;
+  while (dst < _data_end__) {
+    *dst++ = *src++;
+  }
+}
+
+void init_bss() {
+  char *dst = _bss_start__;
+  while (dst < _bss_end__) {
+    *dst++ = 0;
+  }
+}
+
 void crt_entry() {
-  
+  init_data();
+  init_bss();
 }
