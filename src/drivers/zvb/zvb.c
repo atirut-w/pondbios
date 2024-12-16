e954c5d3d92105be048676c9a60dc0ea4adeb558
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-12-16 11:03:32 +0700

    Implement text cursor

--- src/drivers/zvb/zvb.c
+++ src/drivers/zvb/zvb.c
@@ -2,6 +2,11 @@
 #include <mmu.h>
 #include <string.h>
 
+#define WIDTH 80
+#define HEIGHT 40
+
+#define ZVB_PERI_BASE 0xa0
+
 static char *tbuff = (char *)0x8000;
 static char *abuff = (char *)0x9000;
 int cursor = 0;
@@ -11,16 +16,28 @@ int puts(const char *s);
 void zvb_init() {
   outb(MMU_PAGE2, 0x40); // Map 0x100000 to 0x8000
   // NOTE: Do not use `memset`, doesn't work on write-only memory
-  for (int i = 0; i < 80 * 40; i++) {
+  for (int i = 0; i < WIDTH * HEIGHT; i++) {
     tbuff[i] = ' ';
     abuff[i] = 0x07;
   }
+  outb(ZVB_PERI_BASE + 6, 0); // Disable hardware cursor
 }
 
 int puchar_impl(int c) {
-  tbuff[cursor] = c;
   abuff[cursor] = 0x07;
-  cursor++;
+
+  switch (c) {
+  default:
+    tbuff[cursor] = c;
+    abuff[cursor] = 0x07;
+    cursor++;
+    break;
+  case '\n':
+    cursor = (cursor / WIDTH + 1) * WIDTH;
+    break;
+  }
+
+  abuff[cursor] = 0x70;
 }
 
 int puts(const char *s) {
